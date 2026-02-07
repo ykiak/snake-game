2ad769357e2542e2891282be7cd717284264dd09
Kaiky Rodrigues <kaikyro11@gmail.com>
2026-02-07 15:13:55 -0300

    feat: implement score and game over - use localStorage to store user's max score - display game over screen with a restart button - disable snake movements until the game restart

--- script/script.js
+++ script/script.js
@@ -1,10 +1,42 @@
-import { drawSnake, moveSnake, growSnake } from "./snake.js"
+import { drawSnake, moveSnake, growSnake, killSnake, drawSnakeAfterDeath } from "./snake.js"
 
-const canvas = document.querySelector("canvas")
+export const canvas = document.querySelector("canvas")
 export const ctx = canvas.getContext("2d")
 export const size = 30
 
+const storagedMaxScore = localStorage.getItem("score") || 0
+
+const score = document.querySelector(".score")
+const maxScore = document.querySelector(".max-score")
+const gameOver = document.querySelector(".game-over")
+const restartButton = document.querySelector("#restart")
+maxScore.innerHTML = storagedMaxScore
+
+document.addEventListener("death", () => {
+    score.innerHTML = "00"
+    gameOver.style.display = "flex"
+    canvas.style.filter = "blur(2px)"
+})
+
+document.addEventListener("eat", () => {
+    score.innerHTML = parseInt(score.innerHTML) + 10
+    isMaxScore()
+    maxScore.innerHTML = localStorage.getItem("score")
+})
+
+restartButton.addEventListener("click", () => {
+    gameOver.style.display = "none"
+    canvas.style.filter = "none"
+    drawSnakeAfterDeath()
+})
+
 let loop
+
+const isMaxScore = () => {
+    if(storagedMaxScore < parseInt(score.innerHTML)){
+        localStorage.setItem("score", parseInt(score.innerHTML))
+    }
+}
 
 const gameLoop = () => {
     clearTimeout(loop)
@@ -12,8 +44,9 @@ const gameLoop = () => {
     
     moveSnake()
     drawSnake()
+    killSnake()
     growSnake()
-    
+
     loop = setTimeout(() => {
         gameLoop()
     }, 100)
--- script/snake.js
+++ script/snake.js
@@ -1,11 +1,12 @@
-import { ctx, size } from "./script.js"
+import { ctx, size, canvas } from "./script.js"
 import { drawFood, food } from "./food.js"
 import { randomColor, randomPosition } from "./utils.js"
 
-const snake = [
+let snake = [
     { x: 30, y: 30 },
     { x: 60, y: 30 },
 ]
+let isSnakeDeath = false
 let direction
 
 export const drawSnake = () => {
@@ -31,29 +32,58 @@ export const moveSnake = () => {
 }
 
 export const growSnake = () => {
-    const {red, green, blue} = randomColor()
+    const { red, green, blue } = randomColor()
     let x = randomPosition()
     let y = randomPosition()
-    
-    while(snake.some(e => e.x === x && e.y === y)){
+
+    while (snake.some(e => e.x === x && e.y === y)) {
         x = randomPosition()
         y = randomPosition()
     }
 
     const head = snake[snake.length - 1]
     if (head.x === food.x && head.y === food.y) {
+        document.dispatchEvent(new Event("eat"))
         snake.push(head)
 
-        food.x = x,
-        food.y = y,
+        food.x = x
+        food.y = y
         food.color = `rgb(${red}, ${green}, ${blue})`
 
         drawFood()
     }
 }
 
+export const killSnake = () => {
+    const verticalLimit = canvas.width - size
+    const horizontalLimit = canvas.height - size
+
+    const head = snake[snake.length - 1]
+
+    const wallCollision =
+        head.x < 0 || head.x > horizontalLimit || head.y < 0 || head.y > verticalLimit
+    const selfCollision =
+        snake.some((e, index) => e.x === head.x && e.y === head.y && index !== snake.length - 1)
+
+    if (wallCollision || selfCollision) {
+        document.dispatchEvent(new Event("death"))
+        direction = undefined
+        isSnakeDeath = true
+    }
+}
+
+export const drawSnakeAfterDeath = () => {
+    snake = [
+        { x: 30, y: 30 },
+        { x: 60, y: 30 },
+    ]
+    isSnakeDeath = false
+    drawSnake()
+}
+
 document.addEventListener("keydown", ({ key }) => {
-    if (key === "ArrowRight" && direction !== "left") direction = "right"
+    if(isSnakeDeath) return
+    else if (key === "ArrowRight" && direction !== "left") direction = "right"
     else if (key === "ArrowLeft" && direction !== "right") direction = "left"
     else if (key === "ArrowUp" && direction !== "down") direction = "up"
     else if (key === "ArrowDown" && direction !== "up") direction = "down"
